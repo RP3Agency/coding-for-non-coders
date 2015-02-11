9eba76dabd58a4bd4c449262c69caaef19b39f3c
Kurt Roberts <kroberts@rp3agency.com>
2015-02-11 13:12:58 -0500

    first cut - there are problems with gulp, and there are problems with the gulp-reveal plugin, but time to work on content for a while

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+	markdown = require('gulp-markdown'),
+	reveal = require('gulp-reveal'), 
+	notify = require('gulp-notify'), 
+	express = require('express'),
+	paths = {
+		target: './target/',
+		base: './src/',
+		vendor: './src/vendor/**',
+		markdown: './src/*.md'
+		
+	};
+ 
+gulp.task('markdown', function () {
+  return gulp.src(paths.markdown)
+    .pipe(markdown())
+    .pipe(reveal())
+    .pipe(gulp.dest(paths.target));
+});
+ 
+gulp.task('express', function () {
+  var app = express();
+  app.use(express.static(paths.target));
+  app.listen(5000);
+});
+
+gulp.task('vendor', function () {
+    return gulp.src(paths.vendor, {base: paths.base})
+        .pipe(gulp.dest(paths.target))
+  		.pipe(notify({message: 'vendor copied'}));
+}); 
+ 
+ 
+ 
+gulp.task('default', ['markdown', 'vendor']); 
+ 
+gulp.task('watch', function () {
+  gulp.watch('*.md', ['default', 'express']);
+});
---
+++ src/lesson1.md
@@ -0,0 +1,14 @@
+##Slide 1
+
+Some amazing text
+
+---
+
+##Slide 2
+
+Some more amazing text
+
+---
+
+##Slide 3
+#WOWOWOWOW
